898cc82033db09445dbe494ad6741749fb37f6ab
steven <steven.johnson@drake.edu>
2013-03-31 21:57:09 -0500

    reset to my stuffs.

--- src/edu/drake/questionapp/CategoryActivity.java
+++ src/edu/drake/questionapp/CategoryActivity.java
@@ -11,7 +11,7 @@ public class CategoryActivity extends Activity
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
-		super.onCreate(savedInstanceState);		
+		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_category);
 
