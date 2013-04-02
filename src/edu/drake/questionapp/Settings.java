b8acf4dbc695b44688fd06fac771f0ce2cdc4937
Joseph Bustamante <joseph.bustamante@drake.edu>
2013-04-02 09:41:23 -0500

    fixed the settings

--- src/edu/drake/questionapp/Settings.java
+++ src/edu/drake/questionapp/Settings.java
@@ -4,17 +4,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class Settings extends Activity {
+	private static final String TAG = "Settings";
 	 private ListView mainListView;  
 	private ArrayAdapter<String> listAdapter;  
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
 		//Get the ListView resource
@@ -29,7 +37,52 @@ public class Settings extends Activity {
 		 
 		 //Set the Array adapter as the List views adapter
 		 mainListView.setAdapter(listAdapter);
+		 //mainListView.setOnClickListener(onListItemClick);
+		 mainListView.setOnItemClickListener( new OnItemClickListener() {
+			 public void onItemClick(AdapterView<?> arg1, View v, int position, long id) 
+			 {
+		          Toast.makeText(getBaseContext(),"Selected item is "+ position, Toast.LENGTH_SHORT).show();   
+		          switch (position)
+		          {
+		          case 0:
+		        	  Intent i = new Intent(Settings.this, EditProfile.class);     
+			          Settings.this.startActivity(i);
+			          return;
+		          case 1:
+		        	  Intent i2 = new Intent(Settings.this,ChangeEmail.class);     
+			          Settings.this.startActivity(i2);
+			          return;
+		          case 2:
+		        	  Intent i3 = new Intent(Settings.this,ChangeProfile.class);     
+			          Settings.this.startActivity(i3);
+			          return;
+		  
+		        	  
+		          }
+
+			 }
+			 
+		 } );
+		 
+		 
+		 
+		 
 	}
+	 public void onListItemClick(AdapterView<?> arg1, View v, int position, long id) 
+	 {
+		 Log.d(TAG, "This is working in the list clicks for settings ");
+		 
+	
+	          Toast.makeText(getBaseContext(),"Selected item is "+ position, Toast.LENGTH_SHORT).show();    
+//	          Intent i = new Intent(Settings.this, EditProfile.class);     
+//	          Settings.this.startActivity(i);
+			 
+	  
+//				Intent intent = new Intent(this, Settings.class);
+//				startActivity(intent);
+
+	 }
+	 
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,5 +90,6 @@ public class Settings extends Activity {
 		getMenuInflater().inflate(R.menu.settings, menu);
 		return true;
 	}
+	
 	
 }
