215b0a0115cfdd08dced87ccee87848a16c1a5a8
steven <iamfolds@hotmail.com>
2013-04-13 17:23:41 -0500

    remove test

--- src/edu/drake/questionapp/MainActivity.java
+++ src/edu/drake/questionapp/MainActivity.java
@@ -35,8 +35,6 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 	 */
 	ViewPager mViewPager;
 	PhotoFragment piFrag;
-	
-	// testing !
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
