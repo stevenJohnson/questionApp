220d69ea40e6e0cf3a622b773dd4c66144c6c4ed
steven <iamfolds@hotmail.com>
2013-04-17 15:16:18 -0500

    removed logs, added some comments for the question posting

--- src/edu/drake/questionapp/TempQuestionActivity.java
+++ src/edu/drake/questionapp/TempQuestionActivity.java
@@ -1,6 +1,5 @@
 package edu.drake.questionapp;
 
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
@@ -18,7 +17,6 @@ import utilities.ThisApplication;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.Application;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -44,7 +42,8 @@ public class TempQuestionActivity extends Activity {
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.temp_question, menu);
 		return true;
@@ -55,20 +54,18 @@ public class TempQuestionActivity extends Activity {
 		int personId = Integer.parseInt(((EditText)findViewById(R.id.personid)).getText().toString());
 		String question = ((EditText)findViewById(R.id.questionfield)).getText().toString();
 		String appUser = ((ThisApplication)getApplicationContext()).getUsername();
-		
+
 		String filepath = "";
 		//make text file to be uploaded
 		try
 		{
-			Log.d("TempQ", "a");
 			filepath = getApplicationContext().getFilesDir().getPath().toString() + "/tmp.txt";
 			File file = new File(filepath);
-			Log.d("TempQ", "b");
 			file.createNewFile();
-			Log.d("TempQ", "c");
-			
+
 			OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(file));
-			
+
+			// write the question.txt file locally as tmp.txt
 			writer.write(question);
 			writer.write(System.getProperty("line.separator"));
 			writer.write(Integer.toString(personId));
@@ -76,17 +73,15 @@ public class TempQuestionActivity extends Activity {
 			writer.write("0");
 			writer.write(System.getProperty("line.separator"));
 			writer.write(appUser);
-			Log.d("TempQ", "d");
-			
+
 			writer.flush();
 			writer.close();
 		}
 		catch(Exception ex)
 		{
 			// note error in local file creation
-			Log.d("TempQ", ex.getMessage());
+			Log.d("TempQuestion", ex.getMessage());
 		}
-
 
 		//ssh
 		JSch jsch = new JSch();
@@ -100,65 +95,41 @@ public class TempQuestionActivity extends Activity {
 			Session session=jsch.getSession(user, host, port);
 			JSch.setConfig("StrictHostKeyChecking", "no");
 			session.setPassword(pass);
-			Log.d("TempQ", "e");
 			session.connect();
-			Log.d("TempQ", "f");
 			Channel channel=session.openChannel("sftp");
-			Log.d("TempQ", "g");
 			channel.connect();
-			Log.d("TempQ", "h");
 			ChannelSftp c=(ChannelSftp)channel;
-			
+
 			c.cd("WhatWould");
-			
+
+			// obtain vector from ls simply to count the number of directories
 			Vector v = c.ls("Questions");
-			
+
 			c.cd("Questions");
-			Log.d("TempQ", "h");
 			//get question id
-			
-			// one for ., one for ..
+
+			// one for ., one for .., so subtract two since we index from 0
 			int i = v.size() - 2;
-			Log.d("TempQ", "" + i);
 			String questionId = IdConverter.intToStringId(i);
-			Log.d("TempQ", "qid: " + questionId);
-			Log.d("TempQ", "i");
-			
-			
-			/* may not need this file !!!
-			InputStream is = chan.get("number.txt");
-			Scanner scanny = new Scanner(is);
-			is.close();
-			
-			int qId = Integer.parseInt(scanny.nextLine());
-			String questionId = IdConverter.intToStringId(qId);
-			*/
+
+			// make a new directory for the question
 			c.mkdir(questionId);
 			c.cd(questionId);
-			Log.d("TempQ", "in questionid dir");
 			c.mkdir("Answers");
 			c.put(filepath, "question.txt");
-			Log.d("TempQ", "put dat file");
-			
-			
-			
+
 			// add this question id to the user's question.txt file
 			c.cd("../../Users/");
-			Log.d("asdf", "here");
 			c.cd(appUser);
-			Log.d("asdf", "hurrr");
-			
+
 			//make a new file that is a copy of the user's old question.txt
 			filepath = getApplicationContext().getFilesDir().getPath().toString() + "/tmp.txt";
 			File file = new File(filepath);
-			Log.d("asdf", "about to create new file");
 			file.createNewFile();
-			
+
 			OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(file));			
-			
-			Log.d("asdf", "before get questions.txt");
+
 			InputStream is = c.get("questions.txt");
-			Log.d("asdf", "get questions.txt");
 			Scanner scanny = new Scanner(is);
 			String s = "";
 			while(scanny.hasNext())
@@ -169,36 +140,33 @@ public class TempQuestionActivity extends Activity {
 			}
 			scanny.close();
 			is.close();
-			
+
 			// write new questionid
 			writer.write(questionId);
 
 			writer.flush();
 			writer.close();
-			
+
 			// put back the new file
-			Log.d("asdf", "about to rm");
 			c.rm("questions.txt");
-			Log.d("asdf", "rm");
 			c.put(filepath, "questions.txt");
-			Log.d("asdf", "put new user question.txt!!");
 		}
 		catch(Exception ex)
 		{
 			Log.d("TMP QUESTION",ex.getMessage());
 		}
 
-
 		return true;
 	}
-	
-	public class PostQuestionTask extends AsyncTask<Void, Void, Boolean> {
+
+	public class PostQuestionTask extends AsyncTask<Void, Void, Boolean>
+	{
 		@Override
 		protected Boolean doInBackground(Void... params)
 		{
 			return postQuestion();
 		}
-		
+
 		@Override
 		protected void onPostExecute(final Boolean success)
 		{
